f0aba830213e5c6cf4944482e9b8fa650e6feb74
Ifeoluwa-SE <ifeoluwafawole.freelance@gmail.com>
2022-10-18 23:18:19 +0000

    Solutions to C funtions tasks

--- 0x02-functions_nested_loops/11.c
+++ 0x02-functions_nested_loops/11.c
@@ -1,18 +1,19 @@
-#ifdef MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
 int _abs(int);
+int add(int, int);
+int _isalpha(int c);
+int _islower(int c);
+int _putchar(char c);
+int print_sign(int n);
+int add(int a, int b);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
-int add(int a, int b);
 void print_to_98(int n);
+void print_alphabet(void);
 void print_times_table(int n);
+void print_alphabeth_x10(void);
 
 #endif
